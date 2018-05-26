f5413583882791622c5d278cc0d36597d90d6e44
rifayath <39398566+rifayath@users.noreply.github.com>
2018-05-26 14:56:11 +0530

    mytryItem

---
+++ items.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class MytryItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    page_heading = scrapy.Field()
+    page_title = scrapy.Field()
+    #page_link = scrapy.Field()
+    page_content = scrapy.Field()
+    page_content_block = scrapy.Field()
+
+    #image_url = scrapy.Field()
+    image = scrapy.Field()
+    pass
